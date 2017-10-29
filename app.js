c2480bcfc2953ba7bfec0047123dd1efa0363118
wonjae <dldnjswo19@gmail.com>
2017-10-30 00:10:00 +0900

    change currency api

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ var app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+var d = new Date()
 
+global.date = d.getDate()
 // Route setting
 // Auth Middleware - This will check if the token is valid
 // Only the requests that start with /api/v1/* will be checked for the token.
 // Any URL's that do not follow the below pattern should be avoided unless you
 // are sure that authentication is not needed
-app.use('/',require('./routes'))
+app.use('/', require('./routes'))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/apiManager.js
+++ routes/apiManager.js
@@ -29,27 +29,41 @@ module.exports.keyBoard = function (req, res) {
 }
 
 module.exports.parseMessage = function (req, res) {
-  getCurrency(req, res, function (currency, time) {
+
+  var d = new Date()
+  var now = d.getDate()
+  var callback = function () {
     var massage = {
       'message': {
-        'text': '현재 ' + time + "기준 엔화 환율은 " + currency + " 입니다"
+        'text': '오늘의 엔화 환율은 ' + global.currency + ' 입니다'
       },
       'keyboard': {
         'type': 'buttons',
-        'buttons': ['일본 환율','감사합니다티티','무운도!']
+        'buttons': ['일본 환율', '감사합니다티티', '무운도!']
       }
     }
     res.set({
       'content-type': 'application/json'
     }).send(JSON.stringify(massage))
-  })
+  }
+
+  // 이미 했을 때 날짜가 같으면 ㅃㅃ 다르면 날짜 다시 등록하고 뒤의 함수 실행
+  if (global.already) {
+    if (global.date === now) {
+      callback()
+      return
+    }
+    else global.date = now
+  }
+
+  getCurrency(req, res, callback)
 }
 
 function getCurrency (req, res, callback) {
 
   var options = {
     method: 'GET',
-    url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22KRW%22%2C%22JPY%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
+    url: 'http://apilayer.net/api/live?access_key=08b4f2720ea37d7f02d409b3f8d92bff&currencies=KRW,JPY&format=1',
     headers:
       {
         'cache-control': 'no-cache',
@@ -60,15 +74,16 @@ function getCurrency (req, res, callback) {
 
   request(options, function (error, response, body) {
     if (error) throw new Error(error)
-    if(body.error) throw new Error(body.error)
+    if (body.error) throw new Error(body.error)
     console.log(body)
 
-    var data = body.query.results.rate
-    var result = ((data[0].Rate / data[1].Rate) * 100).toFixed(2)
-    var time = data[0].Time
+    var data = body.quotes
+    var result = ((data['USDKRW'] / data['USDJPY']) * 100).toFixed(2)
 
-    console.log('현재 ' + time + " 엔화 환율은 " + result + " 입니다")
-    callback(result, time)
+    console.log('오늘의 엔화 환율은 ' + result + ' 입니다')
+    global.already = true
+    global.currency = result
+    callback(result)
   })
 }
 
