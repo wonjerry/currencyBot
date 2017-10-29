f5557540eea33c466a33315dce928180e0cb3549
wonjae <dldnjswo19@gmail.com>
2017-10-29 22:03:41 +0900

    add calculate currency

--- routes/apiManager.js
+++ routes/apiManager.js
@@ -29,29 +29,23 @@ module.exports.keyBoard = function (req, res) {
 }
 
 module.exports.parseMessage = function (req, res) {
-  var _obj = {
-    user_key: req.body.user_key,
-    type: req.body.type,
-    content: req.body.content
-  }
-
-  console.log(req.body.content)
-
-  var massage = {
-    'message': {
-      'text': '하하하하하'
-    },
-    'keyboard': {
-      'type': 'buttons',
-      'buttons': ['일본 환율', '서비스 준비중']
+  getCurrency(req, res, function (currency, time) {
+    var massage = {
+      'message': {
+        'text': '현재 ' + time + " 엔화 환율은 " + currency + " 입니다"
+      },
+      'keyboard': {
+        'type': 'buttons',
+        'buttons': ['일본 환율']
+      }
     }
-  }
-  res.set({
-    'content-type': 'application/json'
-  }).send(JSON.stringify(massage))
+    res.set({
+      'content-type': 'application/json'
+    }).send(JSON.stringify(massage))
+  })
 }
 
-function getCurrency () {
+function getCurrency (req, res, callback) {
   var options = {
     method: 'GET',
     url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22KRW%22%2C%22JPY%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
@@ -64,11 +58,12 @@ function getCurrency () {
   }
 
   request(options, function (error, response, body) {
-    if (error) throw new Error(error)
+    if (error || body.error) throw new Error(error)
+
     var data = body.query.results.rate
     var result = ((data[0].Rate / data[1].Rate) * 100).toFixed(2)
-    console.log(result)
-    res.json({result: result})
+    var time = data[0].Time
+    callback(result, time)
   })
 }
 
