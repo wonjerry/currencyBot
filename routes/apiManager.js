58da9a466e38daa16d6be9f9977b8291f0c4e5a3
wonjae <dldnjswo19@gmail.com>
2017-10-29 22:40:39 +0900

    for fun

--- routes/apiManager.js
+++ routes/apiManager.js
@@ -36,7 +36,7 @@ module.exports.parseMessage = function (req, res) {
       },
       'keyboard': {
         'type': 'buttons',
-        'buttons': ['일본 환율']
+        'buttons': ['일본 환율','감사합니다티티']
       }
     }
     res.set({
@@ -46,6 +46,7 @@ module.exports.parseMessage = function (req, res) {
 }
 
 function getCurrency (req, res, callback) {
+
   var options = {
     method: 'GET',
     url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22KRW%22%2C%22JPY%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
@@ -58,11 +59,15 @@ function getCurrency (req, res, callback) {
   }
 
   request(options, function (error, response, body) {
-    if (error || body.error) throw new Error(error)
+    if (error) throw new Error(error)
+    if(body.error) throw new Error(body.error)
+    console.log(body)
 
     var data = body.query.results.rate
     var result = ((data[0].Rate / data[1].Rate) * 100).toFixed(2)
     var time = data[0].Time
+
+    console.log('현재 ' + time + " 엔화 환율은 " + result + " 입니다")
     callback(result, time)
   })
 }
